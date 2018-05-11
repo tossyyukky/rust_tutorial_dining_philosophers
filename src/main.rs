40e45ad86612699d63c300bf6b086d6081ff88b1
Toshiyuki Ihara <toshiyuki.ihara@net-marketing.co.jp>
2018-05-11 17:31:25 +0900

    土台

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+    fn eat(&self) {
+        println!("{} is done eating.", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
 }
